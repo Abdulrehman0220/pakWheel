690f5a049d62f9977c4db0caae22f49d404af52f
abd <arrehman0220@gmail.com>
2022-07-07 15:39:29 +0500

    display pics on admin side

--- pages/admin.py
+++ pages/admin.py
@@ -1,12 +1,16 @@
+import img as img
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 from pages.models import Team
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id', 'First_Name', 'Last_Name', 'Designations', 'Date', 'image')
-    list_display_links = ('id', 'First_Name',)
+    def photo(self,object):
+        return format_html('<img src ="{}" width = "40px" style="border-radius:50px"/>'.format(object.image.url))
+    list_display = ('id', 'First_Name', 'Last_Name', 'Designations', 'Date', 'photo')
+    list_display_links = ('id', 'First_Name','Last_Name','photo')
     search_fields = ('First_Name', 'Designations')
     list_filter = ('Designations',)
 
